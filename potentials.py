fe0c996bba030a4d1cee87e023650f7a4e9db26b
Lucas Ortengren <ortengren@wisc.edu>
2026-02-17 18:49:19 -0600

    Cleaning up and vectorizing GB potential

--- potentials.py
+++ potentials.py
@@ -1,6 +1,6 @@
 import ase
 import numpy as np
-from numpy.linalg import norm
+from numpy import linalg as la
 import pandas as pd
 import random
 from scipy.spatial.transform import Rotation
@@ -48,32 +48,23 @@ WALSH_PARAMS = {
 
 
 def gb_shape_function(uhat1, uhat2, rhat, sigma0, kappa):
-    chi = gb_shape_anisotropy(kappa)
-    term1 = (np.dot(uhat1, rhat) + np.dot(uhat2, rhat))**2 / (1 + chi*np.dot(uhat1, uhat2))
-    term2 = (np.dot(uhat1, rhat) - np.dot(uhat2, rhat))**2 / (1 - chi*np.dot(uhat1, uhat2))
+    chi = (kappa**2 - 1)/(kappa**2 + 1)
+    term1 = (np.vecdot(uhat1, rhat) + np.vecdot(uhat2, rhat))**2 / (1 + chi*np.vecdot(uhat1, uhat2))
+    term2 = (np.vecdot(uhat1, rhat) - np.vecdot(uhat2, rhat))**2 / (1 - chi*np.vecdot(uhat1, uhat2))
     sigma = sigma0 / np.sqrt(1 - (chi / 2)*(term1 + term2))
     return sigma
 
 
-def gb_shape_anisotropy(kappa):
-    return (kappa**2 - 1)/(kappa**2 + 1)
-
-
 def gb_axial_energy(uhat1, uhat2, kappa):
-    chi = gb_shape_anisotropy(kappa)
-    return 1 / np.sqrt(1 - (chi*np.dot(uhat1, uhat2))**2)
+    chi = (kappa**2 - 1)/(kappa**2 + 1)
+    return 1 / np.sqrt(1 - (chi*np.vecdot(uhat1, uhat2))**2)
 
 
 def gb_directional_energy(uhat1, uhat2, rhat, kappa_prime, mu):
-
-    chi_prime = gb_energy_anisotropy(kappa_prime, mu)
-    term1 = (np.dot(uhat1, rhat) + np.dot(uhat2, rhat))**2 / (1 + chi_prime*np.dot(uhat1, uhat2))
-    term2 = (np.dot(uhat1, rhat) - np.dot(uhat2, rhat))**2 / (1 - chi_prime*np.dot(uhat1, uhat2))
+    chi_prime = (kappa_prime**(1/mu) - 1) / (kappa_prime**(1/mu) + 1)
+    term1 = (np.vecdot(uhat1, rhat) + np.vecdot(uhat2, rhat))**2 / (1 + chi_prime*np.vecdot(uhat1, uhat2))
+    term2 = (np.vecdot(uhat1, rhat) - np.vecdot(uhat2, rhat))**2 / (1 - chi_prime*np.vecdot(uhat1, uhat2))
     return 1 - chi_prime*(term1 + term2) / 2
-
-
-def gb_energy_anisotropy(kappa_prime, mu):
-    return (kappa_prime**(1/mu) - 1) / (kappa_prime**(1/mu) + 1)
 
 
 def gb_energy_function(uhat1, uhat2, rhat, eps0, kappa, kappa_prime, mu, nu):
@@ -83,10 +74,11 @@ def gb_energy_function(uhat1, uhat2, rhat, eps0, kappa, kappa_prime, mu, nu):
 
 
 def gb(uhat1, uhat2, r, sigma0, eps0, kappa, kappa_prime, mu, nu):
-    rhat = r / norm(r)
+    rmag = np.expand_dims(la.norm(r, axis=-1), axis=-1)
+    rhat = r / rmag
     eps = gb_energy_function(uhat1, uhat2, rhat, eps0, kappa, kappa_prime, mu, nu)
     sigma = gb_shape_function(uhat1, uhat2, rhat, sigma0, kappa)
-    term = sigma0 / (norm(r) - sigma + sigma0)
+    term = sigma0 / (la.norm(r, axis=-1) - sigma + sigma0)
     return 4 * eps * (term**12 - term**6)
 
 
@@ -102,66 +94,3 @@ GB_PARAMS = {
     "mu": 2.,
     "nu": 1.,
 }
-
-
-# --- Helper Functions for Vectorization ---
-
-# Replaces np.dot(a, b) for arrays of shape (N, 3)
-def vdot(v1, v2):
-    return np.sum(v1 * v2, axis=1)
-
-# Replaces norm(v) for arrays of shape (N, 3)
-def vnorm(v):
-    return norm(v, axis=1)
-
-
-# --- Vectorized Physics Functions ---
-
-def vec_gb_shape_function(uhat1, uhat2, rhat, sigma0, kappa):
-    chi = gb_shape_anisotropy(kappa)
-    # Use vdot instead of np.dot
-    dot_u1_r = vdot(uhat1, rhat)
-    dot_u2_r = vdot(uhat2, rhat)
-    dot_u1_u2 = vdot(uhat1, uhat2)
-    term1 = (dot_u1_r + dot_u2_r) ** 2 / (1 + chi * dot_u1_u2)
-    term2 = (dot_u1_r - dot_u2_r) ** 2 / (1 - chi * dot_u1_u2)
-    sigma = sigma0 / np.sqrt(1 - (chi / 2) * (term1 + term2))
-    return sigma
-
-
-def vec_gb_axial_energy(uhat1, uhat2, kappa):
-    chi = gb_shape_anisotropy(kappa)
-    # Use vdot
-    return 1 / np.sqrt(1 - (chi * vdot(uhat1, uhat2)) ** 2)
-
-
-def vec_gb_directional_energy(uhat1, uhat2, rhat, kappa_prime, mu):
-    chi_prime = gb_energy_anisotropy(kappa_prime, mu)
-    # Use vdot
-    dot_u1_r = vdot(uhat1, rhat)
-    dot_u2_r = vdot(uhat2, rhat)
-    dot_u1_u2 = vdot(uhat1, uhat2)
-
-    term1 = (dot_u1_r + dot_u2_r) ** 2 / (1 + chi_prime * dot_u1_u2)
-    term2 = (dot_u1_r - dot_u2_r) ** 2 / (1 - chi_prime * dot_u1_u2)
-    return 1 - chi_prime * (term1 + term2) / 2
-
-
-def vec_gb_energy_function(uhat1, uhat2, rhat, eps0, kappa, kappa_prime, mu, nu):
-    eps1 = vec_gb_axial_energy(uhat1, uhat2, kappa)
-    eps2 = vec_gb_directional_energy(uhat1, uhat2, rhat, kappa_prime, mu)
-    return eps0 * eps1 ** nu * eps2 ** mu
-
-
-def vec_gb(uhat1, uhat2, r, sigma0, eps0, kappa, kappa_prime, mu, nu):
-    # Vectorized normalization
-    # We must reshape the norm to (N, 1) so we can divide the (N, 3) vector r
-    r_magnitudes = vnorm(r)
-    rhat = r / r_magnitudes[:, np.newaxis]
-
-    eps = vec_gb_energy_function(uhat1, uhat2, rhat, eps0, kappa, kappa_prime, mu, nu)
-    sigma = vec_gb_shape_function(uhat1, uhat2, rhat, sigma0, kappa)
-
-    # Calculate final term
-    term = sigma0 / (r_magnitudes - sigma + sigma0)
-    return 4 * eps * (term ** 12 - term ** 6)
