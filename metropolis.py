a4117ce9a70e48caf571822f7aeeefcaf81f050f
Lucas Ortengren <ortengren@wisc.edu>
2026-02-26 14:47:04 -0600

    Revising and optimizing simulation logic

--- metropolis.py
+++ metropolis.py
@@ -1,7 +1,10 @@
 import numpy as np
 import random
-from trial_moves import simultaneous_move
-from potentials import gb, GB_PARAMS
+
+from pygments.lexers import oldmod
+
+from trial_moves import calc_or_vec, calculate_com_move, calculate_quat_move
+from potentials import gb, quadrupole, GB_PARAMS, QQ
 from enum import Enum
 import random
 from ase.neighborlist import NeighborList
@@ -64,20 +67,32 @@ class MetropolisCalculator:
         )
         self.nl.update(self.current_frame)
 
-    def calc_energy(self, frame, idx):
+    def calc_energy(self, center_idx):
         if self.energy_func == "GB":
-
-            U_GB = 0
-            uhat1 = frame.arrays["or_vec"][idx]
-            for i in range(len(frame)):
-                if i == idx:
-                    continue
-                r = frame.get_distance(idx, i, mic=True, vector=True)
-                if np.linalg.norm(r) > 20:
-                    continue
-                uhat2 = frame.arrays["or_vec"][i]
-                U_GB += gb(uhat1, uhat2, r, *GB_PARAMS.values())
-            return U_GB
+            center_pos = self.current_frame.positions[center_idx].copy()
+            # find neighbors
+            indices, offsets = self.nl.get_neighbors(center_idx)
+            # energy is 0 if center particle has no neighbors
+            if len(indices) == 0:
+                return 0
+            # calculate neighbor positions while respecting pbc
+            cell = self.current_frame.get_cell()
+            shift_vecs = np.dot(offsets, cell)
+            neighbor_positions = self.current_frame.positions[indices] + shift_vecs
+            displacements = neighbor_positions - center_pos
+            # find neighbor orientations
+            center_ell_orientation = self.current_frame.arrays["or_vec"][center_idx].copy()
+            uhat2 = self.current_frame.arrays["or_vec"][indices].copy()
+            uhat1 = np.array([center_ell_orientation for _ in range(len(indices))])
+            assert np.shape(uhat1) == np.shape(uhat2), \
+                f"uhat1 and uhat2 are different shapes \n{np.shape(uhat1)} != {np.shape(uhat2)}"
+            # calculate pairwise interaction energies of center particle w/ each neighbor
+            gb_e = gb(uhat1, uhat2, displacements, *GB_PARAMS.values())
+            qq_e = np.squeeze(quadrupole(uhat1, uhat2, displacements, QQ))
+            pw_energies = gb_e + qq_e
+            # total particle energy contribution is sum over pairs
+            energy = np.sum(pw_energies)
+            return energy
         else:
             return NotImplementedError()
 
@@ -86,21 +101,34 @@ class MetropolisCalculator:
         num_particles = len(self.current_frame)
         rand_idx = random.randint(0, num_particles - 1)
         # calculate particle's contribution to total energy
-        old_energy = self.calc_energy(self.current_frame, rand_idx)
-        # calculate a possible new state and calculate its energy
-        trial_frame = simultaneous_move(self.frames[-1], rand_idx, self.pos_delt, self.or_delt)
-        trial_energy = self.calc_energy(trial_frame, rand_idx)
-        # decide whether trajectory will assume the new state or retain its current state
-        keep_move = decide_accept(old_energy, trial_energy)
+        old_energy = self.calc_energy(rand_idx)
+        # store original position and orientation
+        old_pos = self.current_frame.positions[rand_idx].copy()
+        old_quat = self.current_frame.arrays["or_vec"][rand_idx].copy()
+        old_or_vec = self.current_frame.arrays["or_vec"][rand_idx].copy()
+        # get trial move
+        new_pos = calculate_com_move(self.current_frame.positions[rand_idx], self.pos_delt)
+        new_quat = calculate_quat_move(self.current_frame.arrays["c_q"][rand_idx], self.or_delt)
+        new_or_vec = calc_or_vec(new_quat)
+        # apply trial move
+        self.current_frame.positions[rand_idx] = new_pos
+        self.current_frame.arrays["c_q"][rand_idx] = new_quat
+        self.current_frame.arrays["or_vec"][rand_idx] = new_or_vec
+        self.nl.update(self.current_frame)
+        # calculate new energy
+        new_energy = self.calc_energy(rand_idx)
+        # decide whether simulation will accept trial move
+        keep_move = decide_accept(old_energy, new_energy)
         if keep_move:
-            # add trial state to trajectory
-            self.frames.append(trial_frame)
+            # retain current state
             self.decisions.append(1)
         else:
-            # add a copy of the current state to the trajectory
-            self.frames.append(self.frames[-1])
+            # revert to previous state and update neighborlist
+            self.current_frame.positions[rand_idx] = old_pos
+            self.current_frame.arrays["c_q"][rand_idx] = old_quat
+            self.current_frame.arrays["or_vec"][rand_idx] = old_or_vec
+            self.nl.update(self.current_frame)
             self.decisions.append(0)
-        # update object's step_count variable
         self.step_count += 1
 
     def calculate_trajectory(self, num_steps):
@@ -109,6 +137,7 @@ class MetropolisCalculator:
             # print progress of simulation
             # TODO: use tqdm instead of printing
             if self.step_count % 100 == 0:
+                self.traj.write(self.current_frame)
                 print(self.step_count, " / ", num_steps)
                 acc_rate = np.mean(self.decisions[self.step_count-99:self.step_count+1])
                 print(f"acc_rate: {acc_rate}")
@@ -122,6 +151,8 @@ class MetropolisCalculator:
                     self.pos_delt -= 0.05
                     print(f"or_delt: {self.or_delt+0.05} -> {self.or_delt}")
                     print(f"pos_delt: {self.pos_delt+0.05} -> {self.pos_delt}")
+        # close trajectory file
+        self.traj.close()
             
 
 def calc_free_energy(params):
